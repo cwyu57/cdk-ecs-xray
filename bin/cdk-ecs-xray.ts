ae628a5381fd488d2205f28e444b9202c7e69f6d
cwyu57 <cwyu57@gmail.com>
2021-02-24 18:50:37 +0800

    add env props

--- bin/cdk-ecs-xray.ts
+++ bin/cdk-ecs-xray.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { CdkEcsXrayStack } from '../lib/cdk-ecs-xray-stack';
 
 const app = new cdk.App();
-new CdkEcsXrayStack(app, 'CdkEcsXrayStack');
+new CdkEcsXrayStack(app, 'CdkEcsXrayStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
