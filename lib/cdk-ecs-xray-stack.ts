dba0891cdbff71089bc8bb9e04ca96714c9367b1
cwyu57 <cwyu57@gmail.com>
2021-02-28 13:58:53 +0800

    setup ecs cluster and fargate service

--- lib/cdk-ecs-xray-stack.ts
+++ lib/cdk-ecs-xray-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 
 export class CdkEcsXrayStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', { cidr: '10.0.0.0/16' });
+
+    const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition');
+
+    taskDefinition
+      .addContainer('Container', {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        memoryLimitMiB: 512,
+      })
+      .addPortMappings({ containerPort: 80 });
+
+    const fatgetService = new ecs.FargateService(this, 'FargateService', {
+      cluster,
+      taskDefinition,
+    });
+
+    const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = loadBalancer.addListener('Listener', { port: 80 });
+
+    listener.addTargets('ECS', {
+      port: 80,
+      targets: [fatgetService],
+    });
   }
 }
