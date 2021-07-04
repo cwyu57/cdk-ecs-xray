2a385e93eed17267bf6212f9af28b905ce20f716
cwyu57 <cwyu57@gmail.com>
2021-07-04 16:59:25 +0800

    create x-ray-sample-server farget service

--- lib/cdk-ecs-xray-stack.ts
+++ lib/cdk-ecs-xray-stack.ts
@@ -1,6 +1,8 @@
+import * as path from 'path'
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecrAssets from '@aws-cdk/aws-ecr-assets'
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 
 export class CdkEcsXrayStack extends cdk.Stack {
@@ -10,16 +12,20 @@ export class CdkEcsXrayStack extends cdk.Stack {
     // The code that defines your stack goes here
     const vpc = new ec2.Vpc(this, 'Vpc', { cidr: '10.0.0.0/16' });
 
+    const assets = new ecrAssets.DockerImageAsset(this, 'DockerImageAsset', {
+      directory: path.join(__dirname, '../', 'x-ray-sample-server'),
+    });
+
     const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
 
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition');
 
     taskDefinition
       .addContainer('Container', {
-        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        image: ecs.ContainerImage.fromDockerImageAsset(assets),
         memoryLimitMiB: 512,
       })
-      .addPortMappings({ containerPort: 80 });
+      .addPortMappings({ containerPort: 3000 });
 
     const fatgetService = new ecs.FargateService(this, 'FargateService', {
       cluster,
