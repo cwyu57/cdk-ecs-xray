1e2f9ba57560db9b7e94ec8d2320e6543a2301c7
Chun-Wei Yu <cwyu57@gmail.com>
2021-07-06 20:16:11 +0800

    feat: update x ray sample server based on aws-xray-sdk-node-sample (#5)
    * npm install --save aws-xray-sdk
    
    * npm install --save aws-sdk
    
    * npm install --save mysql
    
    * aws-xray-sdk-node-sample - GET /
    
    * aws-xray-sdk-node-sample - GET /http-request/
    
    * aws-xray-sdk-node-sample - GET /aws-sdk/
    
    * aws-xray-sdk-node-sample - GET /mysql/
    
    * refine output
    
    * add environment variables and role permissions

--- x-ray-sample-server/src/index.ts
+++ x-ray-sample-server/src/index.ts
@@ -1,11 +1,88 @@
 import express from 'express';
+import AWSXRay from 'aws-xray-sdk';
+import AWSSdk from 'aws-sdk'
+
+// Capture all AWS clients we create
+const AWS = AWSXRay.captureAWS(AWSSdk);
+AWS.config.update({ region: process.env.DEFAULT_AWS_REGION });
+
+// Capture all outgoing https requestss
+AWSXRay.captureHTTPsGlobal(require('https'));
+const https = require('https');
+
+// Capture MySQL queries
+const mysql = AWSXRay.captureMySQL(require('mysql'));
+
+const XRayExpress = AWSXRay.express;
 
 const app = express();
 const port = 3000;
 
+app.use(XRayExpress.openSegment('SampleSite'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const seg = AWSXRay.getSegment();
+  const sub = seg!.addNewSubsegment('customSubsegment');
+  setTimeout(() => {
+    sub.close();
+    res.sendFile(`${process.cwd()}/index.html`);
+  }, 500);
 });
+
+app.get('/aws-sdk/', (req, res) => {
+  const ddb = new AWS.DynamoDB();
+  const ddbPromise = ddb.listTables().promise();
+
+  ddbPromise.then(function(data) {
+    res.send(`ListTables result:\n ${JSON.stringify(data)}`);
+  }).catch(function(err) {
+    res.send(`Encountered error while calling ListTables: ${err}`);
+  });
+});
+
+app.get('/http-request/', (req, res) => {
+  const endpoint = 'https://amazon.com/';
+  https.get(endpoint, (response: any) => {
+    response.on('data', () => {});
+
+    response.on('error', (err: any) => {
+      res.send(`Encountered error while making HTTPS request: ${err}`);
+    });
+
+    response.on('end', () => {
+      res.send(`Successfully reached ${endpoint}.`);
+    });
+  });
+});
+
+app.get('/mysql/', (req, res) => {
+  const config = {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  };
+
+  const table = process.env.MYSQL_TABLE;
+
+  if (!config.user || !config.database || !config.password || !config.host || !table) {
+    res.send('Please correctly mysql config');
+    return;
+  }
+
+  const connection = mysql.createConnection(config);
+  connection.query(`SELECT * FROM ${table}`, (err, results, fields) => {
+    if (err) {
+      res.send(`Encountered error while querying ${table}: ${err}`);
+      return;
+    }
+    res.send(`Retrieved the following results from ${table}:\n${JSON.stringify(results, null, 2)}`);
+  });
+
+  connection.end();
+});
+
+app.use(XRayExpress.closeSegment());
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
