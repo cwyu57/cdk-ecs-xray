b28a58caf585addffb13b808d6908e29da8d0306
cwyu57 <cwyu57@gmail.com>
2021-07-06 21:29:43 +0800

    cdk-ecs-xray-request-stream-handler - receive event from dynamo and put into s3

--- lambda/cdk-ecs-xray-request-stream-handler/index.js
+++ lambda/cdk-ecs-xray-request-stream-handler/index.js
@@ -1,5 +1,24 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
 
+const region = process.env.AWS_REGION;
+const bucketName = process.env.BUCKET_NAME;
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  region: region,
+});
+
 exports.handler = async (event) => {
   console.log('event =', JSON.stringify(event, null, 2));
+  await Promise.all(
+    event.Records.map((record) => {
+      return s3.putObject({
+        Bucket: bucketName,
+        Key: record.dynamodb.NewImage.id,
+        Body: {
+          record: record
+        },
+      }).promise();
+    }),
+  );
 };
