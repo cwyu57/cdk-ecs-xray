0e08e4c1e016b3057d00a2bb48516f5493f02992
cwyu57 <cwyu57@gmail.com>
2021-07-06 13:29:00 +0800

    aws-xray-sdk-node-sample - GET /

--- x-ray-sample-server/src/index.ts
+++ x-ray-sample-server/src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import AWSXRay from 'aws-xray-sdk';
+import AWSSdk from 'aws-sdk'
+
+const AWS = AWSXRay.captureAWS(AWSSdk);
+const XRayExpress = AWSXRay.express;
+AWS.config.update({ region: process.env.DEFAULT_AWS_REGION });
 
 const app = express();
 const port = 3000;
 
+app.use(XRayExpress.openSegment('SampleSite'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const seg = AWSXRay.getSegment();
+  const sub = seg!.addNewSubsegment('customSubsegment');
+  setTimeout(() => {
+    sub.close();
+    res.sendFile(`${process.cwd()}/index.html`);
+  }, 500);
 });
+
+app.use(XRayExpress.closeSegment());
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
